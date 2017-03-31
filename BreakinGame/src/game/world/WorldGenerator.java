fb783facded26e86485953cc8c02a08f51ce439d
Idrias <nomail1@gmx.net>
2017-03-31 22:22:02 +0200

    push

--- BreakinGame/src/game/world/WorldGenerator.java
+++ BreakinGame/src/game/world/WorldGenerator.java
@@ -1,13 +1,17 @@
 package game.world;
- 
+
+import java.util.ArrayList;
+import game.world.levels.Level;
+
 /*
- * World: continous stream of levels, connected by tunnels 
+ * World: continous stream of levels 
  * Level: Formation of GameObjects that need to be destroyed / passes to reach next level
- * Tunnel: Connection from exit of one level to entrance of new level
+ * 
+ *(Tunnel: Connection from exit of one level to entrance of new level)
  * 
  * WorldGenerator: Creates the world by instancing prefab levels
  */
 
 public class WorldGenerator {
-
+	ArrayList<Level> world;
 }
---
+++ BreakinGame/src/game/world/levels/L_SimpleWall.java
@@ -0,0 +1,16 @@
+package game.world.levels;
+
+
+public class L_SimpleWall extends Level {
+	
+	L_SimpleWall() {
+		entryX = EVERYWHERE;
+		exitX = EVERYWHERE;
+	}
+
+	@Override
+	boolean inflate(int tryEntryX, int tryExitX) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
